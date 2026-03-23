56a6718526b7b02c3ab120a58c64d5ad8dd261ed
unknown <luisalbertorosalesg@gmail.com>
2026-03-22 23:09:14 -0600

    vista de inicio

---
+++ cursoLaravel/resources/views/inicio.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+</head>
+<body>
+    <h1>Bienvenidos a Laravel 11</h1>
+    <h2>Mi primer blade en laravel</h2>
+    
+</body>
+</html>ml
