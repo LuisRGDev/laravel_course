059bb7aa4a74490f1f1ad4ae3ebd8147334810fa
LuisRGDev <luisalbertorosalesg@gmail.com>
2026-03-27 14:59:58 -0600

    Templates, layouts, extend

--- cursoLaravel/resources/views/suma.blade.php
+++ cursoLaravel/resources/views/suma.blade.php
@@ -1,11 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Suma de 2 numeros</title>
-</head>
-<body>
+
+
+@extends('layouts.app')
+
+@section('content')
+
     <h2>Sumar 2 numeros</h2>
     <form action="/suma" method="POST">
         @csrf
@@ -19,10 +17,9 @@
     </form>
 
     <br>
-    @if(isset($resultado))
+    @if(isset($res))
         
-        <h3>Resultado de la suma: {{$resultado}}</h3>
+        <h3>Resultado de la suma: {{$res}}</h3>
         
     @endif
-</body>
-</html>
+@endsection
--- cursoLaravel/routes/web.php
+++ cursoLaravel/routes/web.php
@@ -4,24 +4,18 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 use App\Http\Controllers\SumaController;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 });
 
-Route::get('/suma', function () {
-    return view('suma');
-});
+// Route::get('/suma', function () {
+//     return view('suma');
+// });
 
-Route::post('/suma', function (Request $request) {
+Route::get('/suma', [SumaController::class, 'index']);
 
-    $num1 = $request->input('num1');
-    $num2 = $request->input('num2');
-    $resultado = $num1 + $num2;
-    echo "La suma de $num1 + $num2 es: $resultado";
-    
-    return view('suma', ['resultado' => $resultado]);
-});
+Route::post('/suma', [SumaController::class, 'suma']);
