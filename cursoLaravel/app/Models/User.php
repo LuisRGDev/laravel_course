bf4951a600e67f68d87a111e1ea46fe49c9b0245
LuisRGDev <luisalbertorosalesg@gmail.com>
2026-04-08 15:01:32 -0600

    Migracion CRUD

--- cursoLaravel/app/Models/User.php
+++ cursoLaravel/app/Models/User.php
@@ -19,9 +19,9 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'name',
         'email',
         'password',
+        'role',
     ];
 
     /**
@@ -31,19 +31,10 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password',
-        'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
+    public function isAdmin(): bool
     {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+        return $this->role === 'admin';
     }
 }
